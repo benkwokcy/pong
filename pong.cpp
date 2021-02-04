c40319c85af649148b6f38ce2554a2b8a31a9bc6
Ben Kwok <ben.kwok.cy@gmail.com>
2021-02-03 19:24:55 -0800

    Factor out wait()

--- pong.cpp
+++ pong.cpp
@@ -6,13 +6,21 @@
 
 using namespace std;
 
+// Wait until at least 16ms have passed, capping the frame rate at 60FPS.
+void wait(const chrono::time_point<chrono::steady_clock>& startTime) {
+    const auto endTime = chrono::steady_clock::now();
+    const auto timeElapsed = chrono::duration<float, milli>(endTime - startTime).count();
+    const auto timeToWait = 16.0f - timeElapsed;
+    if (timeToWait > 0.0) { SDL_Delay(static_cast<unsigned int>(timeToWait)); } 
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argcs[]) {
-    Window window;
+    const Window window;
     Objects objects(window);
     Input input;
 
     while (!input.quit){      
-        auto startTime = chrono::steady_clock::now(); 
+        const auto startTime = chrono::steady_clock::now(); 
 
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -21,11 +29,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argcs[]) {
         objects.update(input);
         objects.draw(window);
 
-        auto endTime = chrono::steady_clock::now();
-        auto timeToWait = 16.0f + chrono::duration<float, milli>(startTime - endTime).count();
-        if (timeToWait > 0.0) { 
-            SDL_Delay(static_cast<unsigned int>(timeToWait)); 
-        } 
+        wait(startTime);
     }
    
     return 0;
