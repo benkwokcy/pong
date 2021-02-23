7386ed4d422f43b449b232e8e9ff75f1450809e6
Ben Kwok <ben.kwok.cy@gmail.com>
2021-02-23 13:28:24 -0800

    Remove unused inputs

--- source/Input.hpp
+++ source/Input.hpp
@@ -20,16 +20,12 @@ struct Input {
                     case SDLK_ESCAPE:   quit = true; break;
                     case SDLK_w:        paddleUp = true; break;
                     case SDLK_s:        paddleDown = true; break;
-                    // case SDLK_UP:       paddleUp = true; break;
-                    // case SDLK_DOWN:     paddleDown = true; break;
                     case SDLK_p:        pause = !pause; break;
                 }
             } else if (event.type == SDL_KEYUP) {
                 switch (event.key.keysym.sym) {
                     case SDLK_w:        paddleUp = false; break;
                     case SDLK_s:        paddleDown = false; break;
-                    // case SDLK_UP:       paddleUp = false; break;
-                    // case SDLK_DOWN:     paddleDown = false; break;
                 }
             }
         }
